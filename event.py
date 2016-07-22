9b099ebe6fc63b64ff29981ca514a442ef728dfb
Ashley Copenhaver <ashleycopenhaver13@gmail.com>
2016-07-22 17:30:49 -0400

    Added a bunch of classes to link up buttons

--- event.py
+++ event.py
@@ -9,12 +9,33 @@ jinja_environment = jinja2.Environment(
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Choose an event!!')
-        template = jinja_environment.get_template(
-            'event.html')
+        template = jinja_environment.get_template('event.html')
         self.response.out.write(template.render())
 
+class PromHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('eventprompage.html')
+        self.response.out.write(template.render())
 
+class WeddingHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('eventweddingpage.html')
+        self.response.out.write(template.render())
+
+class SweetSixteenHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('eventsweetsixteenpage.html')
+        self.response.out.write(template.render())
+
+class BirthdayHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_environment.get_template('eventbirthdaypage.html')
+        self.response.out.write(template.render())
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/prom', PromHandler),
+    ('/wedding', WeddingHandler),
+    ('/sweetsixteen', SweetSixteenHandler),
+    ('/birthday', BirthdayHandler),
 ], debug=True)
